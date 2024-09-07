5516a43cf72a665157ff952bdbf073d02f4c5f30
Clathamr <cameronlatham2000@gmail.com>
2024-09-07 14:51:28 +0100

    Added to repo.

---
+++ Execution_Timer.cpp
@@ -0,0 +1,52 @@
+#include <iostream>     //Input/Output
+#include <fstream>      //Read/Write 
+#include <chrono>       //Time checker
+#include <cstdlib>      //Standard C Library
+using namespace std;    //Simplify
+
+
+int main() {
+
+    string line;                                            // Variable to hold each line of Python entered, rewritten each new line (ends if empty, problem handling whitespace?)
+    ofstream pythonFile("temp_script.py");                  // Output File Stream (OFStream) for code to be written to .py file
+
+
+    //Code Read and write !!! Could also do this with for every line in input - might fix whitespace issue
+    
+    cout << "Enter your Python code (end with an empty line):\n";
+    while (getline(cin, line) && !line.empty()) {           // line assigned string of python code, check line isnt empty or break loop
+        pythonFile << line << endl;                         // Write line through ofstream to pythonFile adding newline chr
+    }
+    pythonFile.close();                                     // Safely close file as it should contain all entered Python now
+
+
+    //Timer Element
+    auto start = chrono::high_resolution_clock::now();      // Check current time, assign to start
+    system("python temp_script.py");                        // Run Python code and prints to terminal
+    auto end = chrono::high_resolution_clock::now();        // Check current time, assign to end
+
+    chrono::duration<double> duration = end - start;        // Calculate start - end, assign to duration
+
+    //Display duration
+    cout << "Execution time: " << duration.count() << " seconds" << endl;   // Display the duration, count method on duration object (double for accuracy) with seconds after it 
+
+    return 0;
+}
+
+
+/* Could be a real problem with duration calculation, as start calculated before execution of code and 
+end calculated after execution. Depends on delay between timechecks and running Python
+
+Also need to rework to handle blank spaces, currently any blank line registers as a break for readwrite loop.
+
+Library seems to be able to handle most of Python, should check if it handles imports (it handles math sqrt) 
+and if there is a wayto put restrictions on it (could handle that in other places if this is able to deal with 
+whatever I throw at it)
+
+Also it runs into some problems with larget loops, seems to just cut off and then not display exec time.
+Either it finishes, calculated and runs out of terminal space to write to (unlikely) or the code cant handle larger loops as it
+
+After some research, i could also display the memory addresses of various things from the python code using a wrapper?
+I dont understand but worth investigating perhaps. 
+
+I could also use this to show the stack, again, not sure how yet. */
